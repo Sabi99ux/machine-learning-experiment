ef2ea4528cc8250e3d5dec37d6eea7cf900b2661
Sabi99ux <sabibachtiar@gmail.com>
2025-08-18 00:57:07 +0800

    Update train.py

--- mechine-learning-car-price-study/MechineLearning-car-price-V2/train.py
+++ mechine-learning-car-price-study/MechineLearning-car-price-V2/train.py
@@ -42,10 +42,11 @@ model = MultiOutputRegressor(XGBRegressor(
     reg_alpha=0.1,
     reg_lambda=1,
     random_state=42,
-    tree_method='hist',
+    tree_method='hist', #You can use tree_method="gpu_hist" if your laptop/PC has a supported NVIDIA GPU with CUDA. Otherwise, XGBoost will fall back to CPU with hist
     eval_metric='rmse'
 ))
 
 model.fit(X_train, y_train)
+
 
 joblib.dump(model, 'Result/MechineLearning-car-price-v2-XGBRegressor.pkl')
